ff9b4d950c8dc9a83878c6da94807d6bfdc9a50d
bprcode <bprcode@gmail.com>
2025-02-15 19:19:27 -0800

    Begin preparing query test environment

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,28 @@
 import dotenv from 'dotenv'
 dotenv.config({ path: '../.lite-secrets/pg-ci/.env'})
 import express, { Request, Response } from 'express'
+import 'express-async-errors'
+import pg from 'pg'
 const app = express()
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Status OK')
-})
+app
+  .get('/', (req: Request, res: Response) => {
+    res.send('Status OK')
+  })
+  .get('/test-query', async (req:Request, res:Response) => {
+    const client = new pg.Client()
+
+    await client.connect()
+    const result = await client.query('SELECT NOW()')
+    console.log('Got NOW() result:', result)
+
+    await client.end()
+  })
+  .use((err: Error, req:Request, res:Response, next:Function) => {
+    console.log('ERROR HANDLER:', err)
+    res.status(500).send('⚠️ Server error: ' + err.message)
+
+  })
 
 let server = app.listen(3000, () => {
   const address = server.address() as unknown as { port: string }
