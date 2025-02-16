c4728638e5c2c229759539cbbc2a754f90ad81fa
bprcode <bprcode@gmail.com>
2025-02-16 15:09:15 -0800

    Perform type narrowing

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,40 @@
 import dotenv from 'dotenv'
-dotenv.config({ path: '../.lite-secrets/pg-ci/.env'})
+dotenv.config({ path: '../.lite-secrets/pg-ci/.env' })
 import express, { Request, Response } from 'express'
 import 'express-async-errors'
 import pg from 'pg'
 const app = express()
 
 app
+
   .get('/', (req: Request, res: Response) => {
     res.send('Status OK')
   })
-  .get('/test-query', async (req:Request, res:Response) => {
+
+  .get('/test-query', async (req: Request, res: Response) => {
     const client = new pg.Client()
 
     await client.connect()
-    const result = await client.query('SELECT NOW()')
-    console.log('Got NOW() result:', result)
+    const result = await client.query('SELECT * FROM fruits')
+    console.log('Got result:', result)
 
     await client.end()
+
+    res.json(result.rows)
   })
-  .use((err: Error, req:Request, res:Response, next:Function) => {
+
+  .use((err: Error, req: Request, res: Response, next: Function) => {
     console.log('ERROR HANDLER:', err)
     res.status(500).send('⚠️ Server error: ' + err.message)
-
   })
 
 let server = app.listen(3000, () => {
-  const address = server.address() as unknown as { port: string }
+  const address = server.address()
+  if(!address || typeof address === 'string') {
+    console.log('Unfamiliar address type:', address)
+    return
+  }
+
   console.log(
     '🦆 Server up: \u001b[36mhttp://localhost:' + address.port + '\u001b[37m'
   )
